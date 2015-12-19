ab7093d02095766bc46ee38e277bae243463db50
unknown <maniksri@NJ2WPLABG439465.corp.avisbudget.com>
2015-12-19 16:54:43 -0500

    Home page done

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('carrentalApp', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,9 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('HomeCtrl', ['$scope', function($scope){
+    $scope.switch_to_locsearch = function(){
+      console.log("Switch to location search page");
+    }
+}])
